26b4d36933dcaaa54a229d4535bfaad6756168b5
wangzhongfu <wangzhongfu@lakala.com>
2017-03-16 16:32:40 +0800

    贝叶斯定理，孩子问题

---
+++ mycode/random_kid.py
@@ -0,0 +1,31 @@
+import math, random
+
+def random_kid():
+    return random.choice(["boy", "girl"])
+
+
+if __name__ == "__main__":
+
+    #
+    # CONDITIONAL PROBABILITY
+    #
+
+    both_girls = 0
+    older_girl = 0
+    either_girl = 0
+
+    random.seed(0)
+    for _ in range(10000):
+        younger = random_kid()
+        older = random_kid()
+        if older == "girl":
+            older_girl += 1
+        if older == "girl" and younger == "girl":
+            both_girls += 1
+        if older == "girl" or younger == "girl":
+            either_girl += 1
+    print(both_girls)
+    print(older_girl)
+    print(either_girl)
+    print("P(both | older):", both_girls / older_girl)      # 0.514 ~ 1/2
+    print("P(both | either): ", both_girls / either_girl)   # 0.342 ~ 1/3
